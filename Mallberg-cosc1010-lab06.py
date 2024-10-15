fa7097f0576cdc7e268852435da87cc29d1f3541
MMallberg <mmallber@uwyo.edu>
2024-10-15 22:16:55 +0000

    Complete lab 06

--- Mallberg-cosc1010-lab06.py
+++ Mallberg-cosc1010-lab06.py
@@ -1,9 +1,9 @@
-# Your Name Here
+# Marit Mallberg
 # UWYO COSC 1010
-# Submission Date
+# 10/15/24
 # Lab 06
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# Lab Section: 10
+# Sources: Serena, t.a.'s etc.
 # your
 # comments
 # here
@@ -79,22 +79,49 @@ print(len(random_string)) # Print out the size for reference
     # You will  need to add the letter to the dictionary on first occurrence 
     # Then increment its corresponding count 
 
+counts={}
+
+for letter in random_string:
+    if letter not in counts.keys():
+        counts[letter]=1
+    else:
+        counts[letter]+=1
+
 
 #Load all the elements into a dictionary
 #Will need to first declare a dictionary 
 
 # Output: each letter and its corresponding occurrence in alphabetical order
+print(sorted(counts.items()))
 
 print("*"*75)
 # Output which letter occurred the most 
 
-most_occurred = ""
-least_occurred = ""
+lowest_value=2500
+highest_value=0
+for key,value in counts.items():
+    if value < lowest_value:
+        lowest_value=value
+        lowest_key=key
+    if value > highest_value:
+        highest_value=value
+        highest_key=key
 
-print(f"The letter that occurred the most is {most_occurred}")
+print(f"{lowest_key} is the least ocurring character.")
+print(f"{highest_key} is the most occurring character.")
+
+
+#print(f"The letter that occurred the most is {most_occurred}")
 print("*"*75)
 # Output which letter occurred the least 
-print(f"The letter that occurred the most is {least_occurred}")
-print("*"*75)
+
+
+#print(f"The letter that occurred the most is {least_occurred}")
+#print("*"*75)
 
 # Output what the percentage of the string each character is, again in alphabetical
+
+percentages={}
+for key,value in counts.items():
+    percentages[key]= value/2500*100
+print(sorted(percentages.items()))
